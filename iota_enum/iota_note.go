e1f044cfd180a8049d53373801a933e7cc6869a3
sunyirong <yirongsun@tencent.com>
2021-03-22 16:52:47 +0800

    feat:iota note

---
+++ iota_enum/iota_note.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+const (
+	// 常量组中，后面的如果没有给出类型和值的话，类型和值会复用前一个常量的类型和表达式。
+	Book = iota
+	Close
+	Phone
+)
+
+func main() {
+	fmt.Println(Book, Close, Phone)
+	// iota叫做常量计数器, 主要用来枚举（枚举的时候，常量的值不重要，只要不同就行）
+	// 	1.iota只能在常量组中使用
+	// 2.不同的const定义块互不干扰
+	// 3.没有表达式的常量定义复用上一行的表达式
+	// 4. 从第一行开始，iota从0逐行加一
+	const (
+		a = iota // iota = 0
+		b = 10   // iota = 1
+		c        // c=10, 复用上一个b的表达式，iota仍然加一，iota=2
+		d = iota // iota = 3
+	)
+	fmt.Print(a, b, c, d)
+}
+
+/*
+输出：
+0 1 2
+0 10 10 3
+*/
--- map/map_note.go
+++ map/map_note.go
@@ -2,23 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-	// array 定义
+func initMap() {
+	// array 初始化的两种方式
 	// 第一种方式，使用大括号，可以同时给出初值
 	m1 := map[string]string{}
 	fmt.Printf("%v\n", m1)
 
-	// 第二种方式，使用make函数创建map，不能给初值
+	// 第二种方式，使用make函数创建map，make函数会创建map底层的哈希表
 	m2 := make(map[string]string)
 	m2["m2"] = "v2"
 	fmt.Printf("%v\n", m2)
+}
 
+func zeroValueOfMap() {
+	// map的零值是nil,
+	// 声明时没有给出初始化的话，map为零值，此时底层的哈希表没有分配空间
+	var m1 map[string]string
+	m1["init"] = "hh" // panic: assignment to entry in nil map
+
+}
+
+func keyOfMap() {
+	m1 := map[string]string{
+		"k": "v",
+	}
 	// map的key类型需要定义了==和!=操作, 因为在哈希表的链中要判断是否是已存在的key
 	// 所以array可以作为key, slice不行
 
 	// go不同于python, 取一个不存在的key，会返回value类型的空值，不会抛出异常。可以使用ok来判断key是否存在
 	a := m1["a"]   // 不会抛出异常
-	fmt.Println(a) // a是string的空值--空字符串""
+	fmt.Println(a) // a是string的空值-空字符串""
 
 	a, ok := m1["a"]
 	if ok {
@@ -28,6 +41,12 @@ func main() {
 	}
 
 	delete(m1, "sun") // 删除元素，注意：删除一个不存在的key，不会抛出异常
+}
+
+func main() {
+	initMap()
+	// zeroValueOfMap()
+	keyOfMap()
 
 }
 
